1750e0d17e00a7c98b348736c168ddb85c3f274e
meri-xx <meri.leeworthy@gmail.com>
2020-10-19 14:11:23 +1100

    comments and readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # express-api
 A test API proxy server using Express.js
+
+node.js code using express is in index.js
+this is where api calls are made and endpoints are exposed to the frontend
+
+frontend static files are exposed from /public
--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 console.log('Server starts!!');
 
+//dependencies
 const express = require('express');
 const fetch = require('node-fetch');
 
+//setup HTTP server. if nodemon is installed, run 'nodemon index.js' for live refresh
 const app = express();
 const port = 3000;
 const server = app.listen(port, () => {
   console.log(`listening on port ${port}...`);
 });
 
+//Expose static files in 'public' to client
 app.use(express.static('public'));
-//app.use(express.json({ limit: '1mb' })); //not sure what this does
 
+//Fetch categories list from Radical Directory and expose to client as endpoint
 app.get('/categories', async (req, res) => {
   const api_url = "https://radical.directory/categories.json";
   const fetch_response = await fetch(api_url);
@@ -19,6 +22,7 @@ app.get('/categories', async (req, res) => {
   res.json(fetch_data);
 });
 
+//Fetch directory topic list and expose to client as endpoint
 app.get('/directory-topics', async (req, res) => {
   const api_url = "https://radical.directory/c/6.json";
   const fetch_response = await fetch(api_url);
